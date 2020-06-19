ee99122a781b94c571e3db6a09928378aa7969ab
zhangguolong <zhangguolong@zzjbim.com>
2020-06-19 17:55:16 +0800

    fix:响应式表单

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms'; // 引入响应式表单
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'; // 引入响应式表单
 
 import { AppComponent } from './app.component';
 import { NameEditorComponent } from './name-editor/name-editor.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { NzButtonModule, NzCardModule, NzFormModule, NzInputModule } from 'ng-zorro-antd'; // 引入antd
+
+registerLocaleData(en);
 
 @NgModule({
    declarations: [
@@ -14,9 +23,16 @@ import { ProfileEditorComponent } from './profile-editor/profile-editor.componen
    ],
    imports: [
       BrowserModule,
-      ReactiveFormsModule // 响应式表单
+      ReactiveFormsModule, // 响应式表单
+      FormsModule,
+      HttpClientModule,
+      BrowserAnimationsModule,
+      NzButtonModule,
+      NzCardModule,
+      NzFormModule,
+      NzInputModule
    ],
-   providers: [],
+   providers: [{ provide: NZ_I18N, useValue: en_US }],
    bootstrap: [
       AppComponent
    ]
--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+// import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
@@ -8,12 +9,53 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ProfileEditorComponent {
 
-  profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl('')
+  constructor(private fb: FormBuilder) {}
+
+  // profileForm = new FormGroup({
+  //   firstName: new FormControl(''),
+  //   lastName: new FormControl(''),
+  //   address: new FormGroup({
+  //     street: new FormControl(''),
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     zip: new FormControl('')
+  //   })
+  // });
+
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
+    aliases: this.fb.array([
+      this.fb.control('')
+    ])
   });
+
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  addAlias(e) {
+    e.preventDefault();
+    this.aliases.push(this.fb.control(''));
+  }
 
   onSubmit() {
     console.log(this.profileForm.value);
+  }
+
+  updateProfile(e) {
+    e.preventDefault();
+    this.profileForm.patchValue({
+      firstName: 'Michael',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
   }
 }
