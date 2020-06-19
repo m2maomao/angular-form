23c533d4a8819a5d2e76e06a94eb9d090a21d16a
zhangguolong <zhangguolong@zzjbim.com>
2020-06-19 13:51:57 +0800

    feat:响应式表单

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'; // 引入响应式表单
 
 import { AppComponent } from './app.component';
+import { NameEditorComponent } from './name-editor/name-editor.component';
+import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NameEditorComponent,
+      ProfileEditorComponent
+   ],
+   imports: [
+      BrowserModule,
+      ReactiveFormsModule // 响应式表单
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
