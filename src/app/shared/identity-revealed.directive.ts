24462a020d818da83e1f557e727e566884e0b600
m2maomao <m2maomao@163.com>
2020-06-29 17:45:45 +0800

    fix:验证表单输入

--- src/app/shared/identity-revealed.directive.ts
+++ src/app/shared/identity-revealed.directive.ts
@@ -1,10 +1,18 @@
 import { Directive } from '@angular/core';
+import { ValidatorFn, FormGroup, ValidationErrors, Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
 
+
+export const identityRevealedValidator: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
+  const name = control.get('name');
+  const alterEgo = control.get('alterEgo');
+  return name && alterEgo && name.value === alterEgo.value ? { identityRevealed: true } : null;
+};
 @Directive({
-  selector: '[appIdentityRevealed]'
+  selector: '[appIdentityRevealed]',
+  providers: [{provide: NG_VALIDATORS, useExisting: IdentityRevealedValidatorDirective, multi: true}]
 })
-export class IdentityRevealedDirective {
-
-  constructor() { }
-
+export class IdentityRevealedValidatorDirective implements Validator{
+  validate(control: AbstractControl): ValidationErrors {
+    return identityRevealedValidator(control);
+  }
 }
